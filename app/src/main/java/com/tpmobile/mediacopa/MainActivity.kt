82db9c02dd8e1cfa382cb49c53bf3ff77a3aeb32
lumelamed <lumelamed@gmail.com>
2023-04-28 20:13:17 -0300

    generamos apikey, mostramos mapa

--- app/src/main/java/com/tpmobile/mediacopa/MainActivity.kt
+++ app/src/main/java/com/tpmobile/mediacopa/MainActivity.kt
@@ -3,13 +3,12 @@ package com.tpmobile.mediacopa
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.maps.android.compose.GoogleMap
 import com.tpmobile.mediacopa.ui.theme.MediaCopaTPTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +21,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    GoogleMap(modifier = Modifier.fillMaxSize())
                 }
             }
         }
