b984f6dc4efd3f837280aacd156270f35e2ac0c9
Ezequiel-H <horowitzeze@gmail.com>
2023-05-14 18:03:51 -0300

    feat: can delete and create specific addresses

--- app/src/main/java/com/tpmobile/mediacopa/ui/screens/DireccionesScreen.kt
+++ app/src/main/java/com/tpmobile/mediacopa/ui/screens/DireccionesScreen.kt
@@ -7,15 +7,18 @@ import android.icu.text.CaseMap.Title
 import android.text.Layout
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.ColumnScopeInstance.align
+import androidx.compose.foundation.lazy.LazyColumn
+//import androidx.compose.foundation.layout.ColumnScopeInstance.align
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,7 +28,6 @@ import androidx.compose.ui.res.vectorResource
 import com.tpmobile.mediacopa.R
 import com.google.maps.android.compose.GoogleMap
 
-
 //@Preview(showBackground = true)
 @Composable
 fun DireccionesScreen(navController: NavController) { // hay que comentar los parametros para poder usar el preview
@@ -33,24 +35,30 @@ fun DireccionesScreen(navController: NavController) { // hay que comentar los pa
     Column(
         verticalArrangement = Arrangement.SpaceAround,
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
+    ) {
 
         Text(
             text = "Inserte las direcciones",
             style = MaterialTheme.typography.h5,
-            modifier = Modifier.padding(bottom = 20.dp , top= 20.dp),
-            )
+            modifier = Modifier.padding(bottom = 20.dp, top = 20.dp),
+        )
 
-        var contador by remember { mutableStateOf(0) }
+        var todoListState = remember { mutableStateListOf("", "") }
 
-        repeat(2) {
-            AutoUpdatingTextField();
-        }
-        repeat(contador) {
-           Row() {
-                AutoUpdatingTextField(); // todo mal proque se borra siempre el de abajo, tengo que borrar el que correctponde
+        repeat(todoListState.size) { index ->
+            Row() {
+                TextField(
+                    label = { Text("Direccion") },
+                    value = todoListState[index],
+                    onValueChange = { todoListState[index] = it },
+                    modifier = Modifier.padding(20.dp)
+                )
                 Button(
-                    onClick = { if (contador > 0) {contador--} }
+                    onClick = {
+                        if (todoListState.size > 2) {
+                            todoListState.removeAt(index)
+                        }
+                    }
                 ) {
                     Text(text = "X")
                 }
@@ -58,41 +66,34 @@ fun DireccionesScreen(navController: NavController) { // hay que comentar los pa
         }
 
         FloatingActionButton(
-            onClick = {if (contador < 2) { contador ++ }  },
+            onClick = {
+                if (todoListState.size < 5 ) {
+                    todoListState.add("")
+                }
+            },
             backgroundColor = MaterialTheme.colors.primary,
-            modifier = Modifier.padding(top= 20.dp)
+            modifier = Modifier.padding(top = 20.dp)
             // todo me gusatria poner un enable conatdor >=2 pero no existe
         ) {
             Icon(Icons.Default.Add, contentDescription = "Agregar")
         }
         Button(
-            onClick =  {navController.navigate("Mapa") },
+            onClick = { navController.navigate("Mapa") },
             colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.primary),
             shape = RoundedCornerShape(10.dp),
             modifier = Modifier.padding(5.dp),
         ) {
-            Text(text = "Buscar punto medio" )
+            Text(text = "Buscar punto medio")
         }
     }
-    Column(
-
-    ) {
-
-    }
 }
+
+
+
+
 
 
 @Composable
-fun AutoUpdatingTextField() {
-    var texto by remember { mutableStateOf("") }
-
-    TextField(
-        label = { Text("Direccion") },
-        value = texto,
-        onValueChange = { texto = it },
-        modifier = Modifier.padding(20.dp)
-    )
+fun removeItem(index: Int) {
 
 }
-
-
