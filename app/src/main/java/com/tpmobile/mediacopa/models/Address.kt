f254f5a2755f44e2a2144e0b2ec594de237dfd0b
frandom00 <fdomnguez@frba.utn.edu.ar>
2023-05-25 14:15:58 -0300

    Rearmado el historial para funcionar con el Back, falta comunicacion con el back y con el modulo que calcula el punto medio

--- app/src/main/java/com/tpmobile/mediacopa/models/Address.kt
+++ app/src/main/java/com/tpmobile/mediacopa/models/Address.kt
@@ -3,6 +3,6 @@ package com.tpmobile.mediacopa.models
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.libraries.places.api.model.Place.Type
 
-public class Address(val streetAddress: String?, val latLong: LatLng?, val type: Type?) {
+public class Address(val _id: String?, val streetAddress: String?, val lat: Double?, val long: Double?, val type: Type?, name: String?) {
 
 }
